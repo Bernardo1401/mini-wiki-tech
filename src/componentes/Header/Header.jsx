e4a86c63796681bc4fec1694b4db7c081790b82a
luschettinii <luizaschettinii@gmail.com>
2025-08-12 11:42:45 -0300

    feat: folder next.js 15

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
                 <li><a href="">Framework</a></li>
                 <li><a href="">Meta-framework</a></li>
                 <li><a href="">Bibliotecas de Estilos</a></li>
-                <li><a href="">Next.js 15</a></li>
+                <li><a href="/nextjs15">Next.js 15</a></li>
                 <li><a href="">App Router</a></li>
             </ul>
         </div>
