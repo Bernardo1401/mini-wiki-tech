0d617618cb24b24173a2e746e292e2ea5146ede2
yrkeviin <kevineziquiellima160@gmail.com>
2025-08-12 09:40:44 -0300

    Feat: Create component header

---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,17 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+            <h1>Pesquisa Técnica 🔍</h1>
+
+            <ul>
+                <li><a href="">Framework</a></li>
+                <li><a href="">Meta-framework</a></li>
+                <li><a href="">Bibliotecas de Estilos</a></li>
+                <li><a href="">Next.js 15</a></li>
+                <li><a href="">App Router</a></li>
+            </ul>
+        </div>
+    );
+}
