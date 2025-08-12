ae914a82b052ba5efd12e5aaeb6acb54e2d6e339
yrkeviin <kevineziquiellima160@gmail.com>
2025-08-12 11:21:11 -0300

    Feat: Page metaframework

--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -1,10 +1,13 @@
 import styles from "./Card.module.css";
 
-export default function Card ({ titulo, info }) {
+export default function Card ({ titulo, info, info2, info3, info4 }) {
     return (
         <div className={styles.card}>
             <h2>{titulo}</h2>
             <p>{info}</p>
+            <p>{info2}</p>
+            <p>{info3}</p>
+            <p>{info4}</p>
         </div>
     );
 }
